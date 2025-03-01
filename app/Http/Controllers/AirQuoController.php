f46fb9afbb528f375574143bf114a2a17e233cb1
mike <omodingmike@gmail.com>
2025-03-01 18:12:54 +0300

    added changes.

---
+++ app/Http/Controllers/AirQuoController.php
@@ -0,0 +1,33 @@
+<?php
+
+    namespace App\Http\Controllers;
+
+    use App\Models\Health;
+    use Illuminate\Support\Facades\Http;
+
+    class AirQuoController extends Controller
+    {
+        public function index()
+        {
+            $url      = "https://api.airqo.net/api/v2/devices/measurements/sites/60d058c8048305120d2d614f/recent?token=E494DSTDT3S4MY93";
+            $response = Http::get($url);
+            return response(data_get($response , 'measurements.0.pm2_5.value'));
+        }
+
+        public function actual()
+        {
+            return Health::latest()->take(1)->first();
+//            return Health::all();
+        }
+
+        public function data()
+        {
+            $url      = "https://api.airqo.net/api/v2/devices/measurements/sites/60d058c8048305120d2d614f/recent?token=E494DSTDT3S4MY93";
+            $response = Http::get($url);
+            $data     = [
+                'value'    => data_get($response , 'measurements.0.pm2_5.value') ,
+                'category' => data_get($response , 'measurements.0.aqi_category') ,
+            ];
+            return response($data);
+        }
+    }
